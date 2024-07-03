2253257e4db956a4950aa93bef33d2a00a2a571f
Sandro Kojan <sandro.kojan@postfinance.ch>
2024-07-03 16:05:52 +0200

    initail commit to demonstrate failing openapi-generator > v7.4.0

---
+++ src/main/java/com/example/oas3/Oas3Application.java
@@ -0,0 +1,13 @@
+package com.example.oas3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oas3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oas3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oas3/Oas3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oas3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oas3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
